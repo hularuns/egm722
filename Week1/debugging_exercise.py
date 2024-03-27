c6f3d425455700ac69ec96ffdcb6db9da31e8c9e
hularuns <159131400+hularuns@users.noreply.github.com>
2024-03-27 12:47:34 +0000

    Reorganisation of Week1/
    Small change to debugging_exercise.py
    Moved lake_points shape file into data_files/ sub-directory

--- Week1/debugging_exercise.py
+++ Week1/debugging_exercise.py
@@ -1,17 +1,17 @@
 import random
 
-# pick a random number for the user to guess
+# pick a random number for the user to guess. Changed to 1-20 to match instructions
 rand = random.randint(1, 20)
-print(rand)
+# print(rand) #uncomment to debug
 print('Guess a number between 1 and 20.')
 guess = int(input())  # number needs to be an integer
 
 while guess != rand:  # if the guess is not equal to the random number, you have to guess again
-    if guess == rand:
+    if guess == rand:  # Tells the user if they are correct
         print('Correct!')
-    elif guess < rand:  # if the guess is too high, tell the user.
+    elif guess < rand:  # Tells the user if too low
         print('Too low. Guess again.')
-    else:  # if the guess is too low, tell the user.
+    else:  # if the guess is too high
         print('Too high. Guess again.')
 
     print('Enter a new guess: ')
